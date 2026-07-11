d1d86690c834b5eb3eb11e880d7e8046561476d3
Yen-Xin Wang <yenxin@cosmoslab-5090.lab.internal>
2026-07-11 16:42:46 +0800

    umount debug: add log msg

--- f2fs/inode.c
+++ f2fs/inode.c
@@ -900,8 +900,11 @@ no_delete:
 			atomic_read(&fi->i_compr_blocks));
 
 	if (likely(!f2fs_cp_error(sbi) &&
-				!is_sbi_flag_set(sbi, SBI_CP_DISABLED)))
+				!is_sbi_flag_set(sbi, SBI_CP_DISABLED))){
+		if (is_inode_flag_set(inode, FI_DIRTY_INODE))
+                        printk(KERN_ERR "myf2fs: Evicting DIRTY inode! ino=%lu\n", inode->i_ino);
 		f2fs_bug_on(sbi, is_inode_flag_set(inode, FI_DIRTY_INODE));
+	}
 	else
 		f2fs_inode_synced(inode);
 
